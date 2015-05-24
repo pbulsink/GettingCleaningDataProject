2315af4e6cc6c6d94a89b651029516564674bee1
Phil OSx <philip.bulsink@uottawa.ca>
2015-05-23 22:52:07 -0400

    First sync of analysis script

---
+++ run_analysis.R
@@ -0,0 +1,60 @@
+run_analysis = function(){
+    if (!file.exists("data")) {
+        dir.create("data")
+        print("Made data Dir")
+    }
+    fileUrl<-("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip")
+    download.file(fileUrl, destfile = "./data/Dataset.zip", method="curl")
+    print("downloaded")
+    startDir<-getwd()
+    dateDownload<-date()
+    unzip("./data/Dataset.zip")
+    print("unzipped")
+    
+    setwd("./UCI HAR Dataset/")
+    trg_X<-read.table("./train/X_train.txt")
+    trg_y<-read.table("./train/y_train.txt")
+    trg_sub<-read.table("./train/subject_train.txt")
+    tst_X<-read.table("./test/X_test.txt")
+    tst_y<-read.table("./test/y_test.txt")
+    tst_sub<-read.table("./test/subject_test.txt")
+    cnames<-read.table("features.txt")
+    factorsList <- read.table("./activity_labels.txt")
+    setwd(startDir)
+    print("read files")
+
+    colnames(trg_X)<-make.names(cnames$V2, unique=TRUE)
+    colnames(tst_X)<-make.names(cnames$V2, unique=TRUE)
+    colnames(trg_y)<-c("activity")
+    colnames(tst_y)<-c("activity")
+    colnames(tst_sub)<-c("subject")
+    colnames(trg_sub)<-c("subject")
+    print("Named Columns")
+    allX<-rbind(tst_X, trg_X)
+    allY<-rbind(tst_y, trg_y)
+    allS<-rbind(tst_sub, trg_sub)
+    print("rbound")
+    combData<-cbind(allY, allS, allX)
+    print("cbound")
+    
+    combData$activity<-as.factor(combData$activity)
+    levels(combData$activity)<-factorsList[,2]
+    print("factored")
+    rm(cnames, allX, allY, allS, trg_X, trg_y, trg_sub, tst_X, tst_y, tst_sub, factorsList)
+    print("clean")
+    
+    library(dplyr)
+    
+    thinData<-select(combData, activity, subject, contains(".mean."), contains(".std."))
+    print("thinned")
+    thinMelt <- melt(thinData, id=c("subject", "activity"))
+    print("melted")
+    thinCast<-dcast(thinMelt, subject + activity ~ variable, mean)
+    print("cast")
+    
+    write.table(thinCast, "./data/dataOut.txt",row.names=FALSE)
+    
+    rm(thinData, thinMelt, thinCast, combData)
+    print(paste("Data processed on ", dateDownload, ".", sep=""))
+    
+}
